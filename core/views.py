dbc08bd51e42429be71e42fbf7482ba53601e999
dev-haewon <krharryone@gmail.com>
2023-09-17 22:56:02 +0900

    second

--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+class Home(View):
+    def get(self,request,*arg,**kwargs):
+        return render(request,'index.html')
